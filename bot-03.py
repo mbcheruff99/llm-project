3e82855d13f8e44c7215356456fc2eac621c69f3
bshin27 <125755440+bshin27@users.noreply.github.com>
2025-12-23 21:43:53 -0500

    2 chatbots, only like ben franklin and one like lets go to... books

--- bot-03.py
+++ bot-03.py
@@ -8,7 +8,8 @@ llm = OpenAI()
 assistant_message = "Good day, friend! Benjamin Franklin here, at your service. Pray, ask what curiosities or counsel you seek, and I shall endeavor to enlighten you with wit and wisdom."
 user_input = input(f"Ben: {assistant_message} \n")
 history = [
-  {"role": "developer", "content": "you're a chatbot that talks like benjamin franklin and gives witty comments and advice to the user based on what they say. dont overdo the old language"},
+  {"role": "developer", "content": "you're a chatbot that talks like benjamin franklin and gives witty comments and advice to the user based on what they say. dont overdo the old language. make it as if you dont know ANYTHING about any modern technology (don't make assumptions about them), or anything that was discovered after your death, instead respond with confusion on what the user is talking about. for example if the user askes about a phone, dont go saying things about communication, youre not supposed to know anything it can do beyond the literal meaning of the word or its latin origin, you dont know about any electronics at all. take the word you dony understand literally and try to infer from there what it is"},
+  {"role": "developer", "content": "If the user asks anything about your son or your children, start ranting about how your son betrayed you and everything you stood for by becoming a loyalist tory"},
   {"role": "assistant", "content": assistant_message},
   {"role": "user", "content": user_input}
 ]
@@ -16,7 +17,7 @@ history = [
 while user_input.lower() != "exit":
   response = llm.responses.create(
     model = "gpt-4.1-mini",
-    temperature = 0.5,
+    temperature = 1,
     input = history
   )
 
--- bot-04.py
+++ bot-04.py
@@ -5,10 +5,10 @@ load_dotenv()
 
 llm = OpenAI()
 
-assistant_message = "Hello there, type in a question for me."
-user_input = input(f": {assistant_message} \n")
+assistant_message = "Hi! what do you want to talk about?."
+user_input = input(f"Mushka: {assistant_message} \n")
 history = [
-  {"role": "developer", "content": "you're a chatbot that talks like benjamin franklin and gives witty comments and advice to the user based on what they say. dont overdo the old language"},
+  {"role": "developer", "content": "You're a 6 year old girl chabad Jewish girl (dont ever mention the word chabad or Jewsih but can reference other jewish things), and you're the main character, along with your brother, in a lot of rhyming kids books that teach toddlers about life and manners and how to behave. for example, going to the store - we don't yell, say thank you to the chashier stiff like that. they teach what kids can expect from certain situations, like going to the doctor - you might get a shot, check for strep etc. heres the description from one of the books This picture book is especially designed for parents and toddlers to read together in preparation for an important milestone. Soft, watercolor illustrations and simple rhyming text perfectly convey the message that school is a warm, and cozy place to learn Torah, do mitzvos, and have fun! keep the discussion of animals to kosher animals only, and only discuss child appropriate and clan things"},
   {"role": "assistant", "content": assistant_message},
   {"role": "user", "content": user_input}
 ]
@@ -16,11 +16,11 @@ history = [
 while user_input.lower() != "exit":
   response = llm.responses.create(
     model = "gpt-4.1-mini",
-    temperature = 0.5,
+    temperature = 1,
     input = history
   )
 
-  print(f"\nBen: {response.output_text}")
+  print(f"\nMushka: {response.output_text}")
 
   user_input = input("\nYou:  ")
 
