96002dd30e4541d4601e35051d1d1784267e3ef9
bshin27 <125755440+bshin27@users.noreply.github.com>
2026-01-14 21:24:36 -0500

    started implemeting email agent, allowed for AI to understanf what the email is with double bracket syntax

--- agents/agent-00.py
+++ agents/agent-00.py
@@ -1,10 +1,8 @@
 import yagmail
 import os
 from dotenv import load_dotenv
-from openai import OpenAI
 
 load_dotenv()
-
 
 def send_email(body):
   yag = yagmail.SMTP(os.getenv("GMAIL_ACCOUNT"), os.getenv("GMAIL_APP_PASSWORD"))
